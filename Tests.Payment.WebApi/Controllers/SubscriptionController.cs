47ba50bffb514ea4c44355b892017ea93869b850
bohdan.lushchyk <bohdan.lushchyk@devcom.com>
2020-12-18 17:10:01 +0200

    new payments webapi

---
+++ Tests.Payment.WebApi/Controllers/SubscriptionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tests.Payment.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SubscriptionController : ControllerBase
+    {
+
+        public SubscriptionController()
+        {
+        }
+
+        [HttpGet]
+        public async Task Get()
+        {
+
+        }
+    }
+}
